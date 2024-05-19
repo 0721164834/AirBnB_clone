341eaa73a275d2995c7db51001abab78d31d990d
Rioba <nyakeriphilip2@gmail.com>
2024-05-19 15:56:10 +0300

    debug code for pycodestyle 'line too long'

--- models/base_model.py
+++ models/base_model.py
@@ -23,4 +23,8 @@ class BaseModel:
         return obj_dict
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__,
+                self.id,
+                self.__dict__
+        )
