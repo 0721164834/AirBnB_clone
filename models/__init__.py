2cc2d1bee053154109508cdcfa9fbefd59564302
Rioba <nyakeriphilip2@gmail.com>
2024-05-19 17:39:29 +0300

    doc

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''
+package inistaller
+'''
+
+from models.base_model import BaseModel
+
+classes = {"BaseModel": BaseModel}
--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""
+'''
 BaseModel module for AirBnB clone project.
 Defines a BaseModel class for managing common attributes and methods.
-"""
+'''
 
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
-    """
+    '''
     A base class for other classes in the AirBnb clone project.
     Attributes;
         id (str): Unique identifier for each instance.
         created_at (datetime): Date and time when the instance was created.
         updated_at (datetime): Date and time when the instance was last.
-    """
+    '''
 
     def __init__(self):
         """
@@ -26,17 +27,18 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def save(self):
-        """
-        Updates the Public instance attribute 'updated_at' with the current         """
+        '''
+        Updates the Public instance attribute updated_at with the current
+        '''
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """
+        '''
         Converts the instances to dictionary reprentation.
 
         Returns:
             dict: dictionary cantaing all keys of the instance __dict__
-        """
+        '''
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
@@ -44,9 +46,9 @@ class BaseModel:
         return obj_dict
 
     def __str__(self):
-        """
+        '''
         Returns a strign represnation of the instance
-        """
+        '''
         return "[{}] ({}) {}".format(
                 self.__class__.__name__,
                 self.id,
