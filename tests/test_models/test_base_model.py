7422190a7785279bc4de1509a378dff49915ad2f
Rioba <nyakeriphilip2@gmail.com>
2024-05-19 22:48:07 +0300

    file_documatation

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,21 +1,64 @@
 #!/usr/bin/python3
 """
-Unit tests for BaseModel class.
+Unit tests for the BaseModel class.
 """
+
+import unittest
+from datetime import datetime
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(
-        key,
-        type(my_model_json[key]),
-        my_model_json[key]
-    ))
+
+class TestBaseModel(unittest.TestCase):
+    """
+    Test suite for the BaseModel class.
+    """
+
+    def test_id(self):
+        """
+        Test if the 'id' attribute is a string.
+        """
+        model = BaseModel()
+        self.assertIsInstance(model.id, str)
+
+    def test_created_at(self):
+        """
+        Test if 'created_at' is a datetime object.
+        """
+        model = BaseModel()
+        self.assertIsInstance(model.created_at, datetime)
+
+    def test_updated_at(self):
+        """
+        Test if 'updated_at' is a datetime object.
+        """
+        model = BaseModel()
+        self.assertIsInstance(model.updated_at, datetime)
+
+    def test_save(self):
+        """
+        Test if 'updated_at' is updated by the save method.
+        """
+        model = BaseModel()
+        old_updated_at = model.updated_at
+        model.save()
+        self.assertNotEqual(model.updated_at, old_updated_at)
+
+    def test_to_dict(self):
+        """
+        Test the to_dict method.
+        """
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertEqual(model_dict['id'], model.id)
+        self.assertEqual(model_dict['__class__'], 'BaseModel')
+        self.assertEqual(model_dict['created_at'], model.created_at.isoformat())
+        self.assertEqual(model_dict['updated_at'], model.updated_at.isoformat())
+
+    def test_str(self):
+        """
+        Test the __str__ method.
+        """
+        model = BaseModel()
+        string = str(model)
+        self.assertIn('BaseModel', string)
+        self.assertIn(model.id, string)
